e4636ad68921bfbb44a01c8a5fbf2d3833b2b33f
yuanying <yuanying@fraction.jp>
2012-04-06 14:22:35 +0900

    Added method comment.

--- lib/komo/builder.rb
+++ lib/komo/builder.rb
@@ -2,6 +2,12 @@ require 'komo'
 
 class Komo::Builder
 
+  # Build a site.
+  #
+  # @param [Hash] options
+  # @option options [String] :repository
+  # @option options [String] :branch        (optional)
+  # @option options [String] :working_dir   (optional)
   def build(options={})
     repository  = Grit::Repo.new(File.expand_path(options[:repository]))
     branch      = options[:branch] || 'master'
