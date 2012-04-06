3f683a857fd983ff8acf6e9d03a306a7e52b4f63
yuanying <yuanying@fraction.jp>
2012-04-06 14:08:52 +0900

    Changed method signature at Komo::Builder.

--- lib/komo/builder.rb
+++ lib/komo/builder.rb
@@ -1,16 +1,12 @@
 require 'komo'
 
 class Komo::Builder
-  attr_reader :repository
-  attr_reader :working_dir
 
-  def initialize repository, working_dir
-    @repository   = Grit::Repo.new(File.expand_path(repository))
-    @working_dir  = File.expand_path(working_dir)
-  end
+  def build(options={})
+    repository  = Grit::Repo.new(File.expand_path(options[:repository]))
+    branch      = options[:branch] || 'master'
+    working_dir = File.expand_path(options[:working_dir] || Dir.pwd)
 
-  def build!(options={})
-    
   end
 
 end
